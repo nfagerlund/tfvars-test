84e122c834e750eea955302f130e5ec1b796609a
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2022-07-29 14:14:50 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+variable "animal" {
+  type      = string
+  sensitive = false
+}
+
+variable "vegetable" {
+  type      = string
+  sensitive = false
+}
+
+output "animal-variable" {
+  value = var.animal
+}
+
+output "vegetable-variable" {
+  value = var.vegetable
+}
